4d70a8f7874cba0ffe3c563e853a153b090b96de
jeel <jeelmaheshwari2006@gmail.com>
2026-05-04 19:29:22 +0530

    createPost , like and createdPost routes created and controller to for that

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,10 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+import Post from '../models/Post.js'
+import Comment from '../models/Comment.js'
+import Like from '../models/Like.js'
+import {useAuth} from '../middleware/useAuth.js'
 
 export const getPage = (req, res) => {
     res.send('Hello world')
@@ -50,4 +55,72 @@ export const login = async (req, res) => {
     )
 
     res.json({message: 'Login successful', token})
+}
+
+export const createPost = async (req, res) => {
+    const {content} = req.body
+    const userId = req.user.userId
+
+    if (!content) {
+        return res.status(400).json({message: 'Content is required'})
+    }
+
+    const user = await User.findById(userId)
+    if (!user) {
+        return res.status(400).json({message: 'User not found'})
+    }
+
+    const post = await Post.create({text: content, user: userId})
+    res.json({message: 'Post created successfully', post})
+
+}
+
+export const createComment = async (req, res) => {
+    
+    const {content} = req.body
+    
+    if (!content) {
+        return res.status(400).json({message: 'Content is required'})
+    }
+
+    const userId = req.user.userId
+    const user = await User.findById(userId)
+    if (!user) {
+        return res.status(400).json({message: 'User not found'})
+    }
+    
+    const postId = req.params.postId 
+    const post = await Post.findById(postId)
+    if (!post) {
+        return res.status(400).json({message: 'Post not found'})
+    }
+
+    const comment = await Comment.create({text: content, user: userId, post: postId})
+    res.json({message: "comment created successfully", comment})
+    
+}
+
+export const likePost = async (req, res) => {
+    const userId = req.user.userId
+    const user = await User.findById(userId)
+
+    if (!user) {
+        return res.status(400).json({message: 'User not found'})
+    }
+
+    const postId = req.params.postId
+    const post = await Post.findById(postId)
+
+    if (!post) {
+        return res.status(400).json({message: 'Post not found'})
+    }
+
+    const existingLike = await Like.findOne({user: userId, post: postId})
+
+    if (existingLike) {
+        return res.status(400).json({message: 'Like already exists by this user for this post'})
+    }
+
+    const like = await Like.create({user: userId, post: postId})
+    res.json({message: 'Post liked successfully', like})
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router()
 import {useAuth} from '../middleware/useAuth.js'
-import {getPage, register, login} from '../controller/userController.js'
+import {getPage, register, login, createPost, createComment, likePost} from '../controller/userController.js'
 
 router.get('/', getPage)
 
@@ -9,8 +9,10 @@ router.post('/register', register)
 
 router.post('/login', login)
 
-router.get('/profile', useAuth, async (req, res) => {
-    res.json({message: 'This is a protected route', user: req.user})
-})
+router.post('/createPost', useAuth, createPost)
+
+router.post('/post/:postId/comment', useAuth, createComment)
+
+router.post('/post/:postId/like', useAuth, likePost)
 
 export default router
