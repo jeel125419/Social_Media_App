36b03e9e82a625012113c296aa7396dc86ea9892
jeel <jeelmaheshwari2006@gmail.com>
2026-06-08 18:21:39 +0530

    changes in navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,20 +14,18 @@ function App() {
 
   return (
     <>
-      <nav className='flex gap-4 p-4 bg-gray-200 fixed top-4 left-4 right-4 rounded-md z-10 justify-end'>
-        <Link to="/" className="bg-amber-500 cursor-pointer hover:scale-125 transition-transform rounded-md p-2 hover:bg-amber-600">Home</Link>
-        {!token && <Link to="/register" className="bg-amber-500 cursor-pointer hover:scale-125 transition-transform rounded-md p-2 hover:bg-amber-600">Register</Link>}
-        {!token && <Link to="/login" className="bg-amber-500 cursor-pointer hover:scale-125 transition-transform rounded-md p-2 hover:bg-amber-600">Log In</Link>}
-        {token && <Link to="/dashboard" className="bg-amber-500 cursor-pointer hover:scale-125 transition-transform rounded-md p-2 hover:bg-amber-600">Dashboard</Link>}
+      <nav className='flex gap-4 p-2 bg-white fixed top-4 left-4 right-4 rounded-md z-10 justify-between'>
+        <div className='flex gap-8 justify-center items-center'>
+        <Link to="/" className="cursor-pointer hover:bg-[#DBEAFE] p-2 rounded-2xl text-[#475569] hover:text-[#2563EB] border border-transparent border-[#E2E8F0] hover:border-[#E2E8F0] hover:box-border transition-all duration-200">Home</Link>
+        {!token && <Link to="/register" className="cursor-pointer hover:bg-[#DBEAFE] p-2 rounded-2xl text-[#475569] hover:text-[#2563EB] border border-transparent hover:border-[#E2E8F0] hover:box-border transition-all duration-200">Register</Link>}
+        {!token && <Link to="/login" className="cursor-pointer hover:bg-[#DBEAFE] p-2 rounded-2xl text-[#475569] hover:text-[#2563EB] border border-transparent hover:border-[#E2E8F0] hover:box-border transition-all duration-200">Log In</Link>}
+        {token && <Link to="/dashboard" className="cursor-pointer hover:bg-[#DBEAFE] p-2 rounded-2xl text-[#475569] hover:text-[#2563EB] border border-transparent hover:border-[#E2E8F0] hover:box-border transition-all duration-200">Dashboard</Link>}
+
+        </div>
 
         {
-          token && <button onClick={()=> {
-            localStorage.removeItem('token')
-            localStorage.removeItem('username')
-            navigation('/')
-          }} className="bg-red-600 cursor-pointer hover:scale-125 transition-transform rounded-md p-2 hover:bg-red-700">Log Out</button>
+          token && <div onClick={() => navigation('/login')} className="cursor-pointer h-10 w-10 bg-[#CBD5E1] text-[#334155] rounded-full flex items-center justify-center">J</div>
         }
-
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,19 @@
 import {useState} from 'react'
 import api from '../api/axios'
+import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
-  
+  const navigation = useNavigate()
+  const token = localStorage.getItem('token')
   return (
-    <div>
-        this is dashboard 
+    <div className='flex justify-center items-center bg-[#F8FAFC] h-screen'>
+        {
+          token && <button onClick={()=> {
+            localStorage.removeItem('token')
+            localStorage.removeItem('username')
+            navigation('/')
+          }} className="bg-red-600 cursor-pointer hover:scale-125 transition-transform rounded-md p-2 hover:bg-red-700">Log Out</button>
+        }
     </div>
   )
 }
